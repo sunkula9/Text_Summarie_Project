def47177fd29cf49534d3961587a810d89cf97af
sunkula9 <maheshbabu.sunkula@gmail.com>
2025-03-24 15:50:05 +0530

    foldera are edited

--- template.py
+++ template.py
@@ -18,7 +18,7 @@ list_of_files = [
     f"src/{project_name}/config/configuration.py",
     f"src/{project_name}/pipeline/__init__.py",
     f"src/{project_name}/entity/__init__.py",
-    f"sec/{project_name}/constants/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
     "config/config.yaml",
     "params.yaml",
     "app.py",
